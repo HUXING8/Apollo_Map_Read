d7d6e6fd8f92815bf7c868cea418f3c46ae06842
huxing <huxing1@zbmec.com>
2024-07-26 14:43:01 +0800

    update file

--- load_map.py
+++ load_map.py
@@ -1,7 +1,6 @@
 import output.map_pb2
 from google.protobuf import text_format
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
 def get_pb_from_text_file(filename, pb_value):
     """Get a proto from given text file."""
@@ -10,6 +9,7 @@ def get_pb_from_text_file(filename, pb_value):
 
 
 def draw_lane_boundary(lane, ax, color_val):
+    '''draw boundary'''
     for curve in lane.left_boundary.curve.segment:
         if curve.HasField('line_segment'):
             px = []
@@ -28,17 +28,19 @@ def draw_lane_boundary(lane, ax, color_val):
             ax.plot(px, py, ls='-', c=color_val, alpha=0.5, picker=True)
 
 
-def draw_lane_central(lane, ax, color_val, alpha_val = 0.5):
+def draw_lane_central(lane, ax, color_val):
+    '''draw boundary'''
     for curve in lane.central_curve.segment:
         if curve.HasField('line_segment'):
             px = []
             py = []
-        for p in curve.line_segment.point:
-            px.append(float(p.x))
-            py.append(float(p.y))
-        line2d, = ax.plot(px, py, ls='-', linewidth=5, c=color_val, alpha=alpha_val, picker=True)
+            for p in curve.line_segment.point:
+                px.append(float(p.x))
+                py.append(float(p.y))
+            line2d, = ax.plot(px, py, ls='-', linewidth=5, c=color_val, alpha=0.5, picker=True)
 
 def draw_polygon_boundary(polygon, ax, color_val):
+    '''draw polygon boundary'''
     px = []
     py = []
     for point in polygon.point:
@@ -59,12 +61,16 @@ def draw_stop_line(line_segment, ax, color_val):
       py.append(float(p.y))
     ax.plot(px, py, 'o-', linewidth=1, c=color_val, picker=True)
 
-def draw_polygon(polygon, ax, color_val):
-    pxy = []
+def draw_polygon_boundary(polygon, ax, color_val):
+    """draw polygon boundary"""
+    px = []
+    py = []
     for point in polygon.point:
-      pxy.append([point.x, point.y])
-    polygon = patches.Polygon(pxy, True)
-    ax.add_patch(polygon)
+        px.append(point.x)
+        py.append(point.y)
+    ax.plot(px, py, ls='-', c=color_val, alpha=0.5)
+    ax.plot([px[-1],px[0]], [py[-1],py[0]], ls='-', c=color_val, alpha=0.5)
+
 
 if __name__ == "__main__":
     map_pb = output.map_pb2.Map()
@@ -72,9 +78,8 @@ if __name__ == "__main__":
 
     fig,ax = plt.subplots()
 
-    # customize your interested data
     for i,lane in enumerate(res.lane):
-        draw_lane_central(lane, ax, 'red', alpha_val = 0.5)
+        draw_lane_central(lane, ax, 'red')
         draw_lane_boundary(lane, ax, 'green')
 
     for junction in res.junction:
@@ -86,13 +91,14 @@ if __name__ == "__main__":
                 draw_stop_line(curve.line_segment, ax, "tomato")
 
     for crosswalk in res.crosswalk:
-        draw_polygon(crosswalk.polygon, ax, 'c')
+        draw_polygon_boundary(crosswalk.polygon, ax, 'c')
 
     for stop_sign in res.stop_sign:
       for stop_line in stop_sign.stop_line:
         for curve in stop_line.segment:
             draw_stop_line(curve.line_segment, ax, "tomato")
     
+    plt.axis('equal')
     plt.show()
     # plt.savefig("map_img.png",dpi=200)
 
